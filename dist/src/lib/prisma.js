24aa8d4319dbc85787d6f71206218b27bb300608
luis-valverde120 <luis.valverde@upec.edu.ec>
2025-05-25 00:51:07 -0500

    fix: route of vercel.json

--- dist/src/lib/prisma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const client_1 = require("@prisma/client");
-exports.prisma = global.prisma || new client_1.PrismaClient();
-if (process.env.NODE_ENV !== "production") {
-    // Prevents creating a new Prisma Client for every API request in development
-    global.prisma = exports.prisma;
-}
